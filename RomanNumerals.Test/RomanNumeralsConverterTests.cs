4bc1dc8a0cec4a3efaece8d39a38d55e25a165d3
Dale Palmer <dalepalmer@outlook.com>
2021-08-23 20:02:00 +0100

    Added functional implementation.

---
+++ RomanNumerals.Test/RomanNumeralsConverterTests.cs
@@ -0,0 +1,100 @@
+using NUnit.Framework;
+
+namespace RomanNumerals.Test
+{
+    class RomanNumeralsConverterTests
+    {
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(11, "XI")]
+        [TestCase(14, "XIV")]
+        [TestCase(15, "XV")]
+        [TestCase(16, "XVI")]
+        [TestCase(19, "XIX")]
+        [TestCase(20, "XX")]
+        [TestCase(21, "XXI")]
+        [TestCase(30, "XXX")]
+        [TestCase(40, "XL")]
+        [TestCase(49, "XLIX")]
+        [TestCase(50, "L")]
+        [TestCase(51, "LI")]
+        [TestCase(57, "LVII")]
+        [TestCase(74, "LXXIV")]
+        [TestCase(90, "XC")]
+        [TestCase(100, "C")]
+        [TestCase(139, "CXXXIX")]
+        [TestCase(400, "CD")]
+        [TestCase(500, "D")]
+        [TestCase(666, "DCLXVI")]
+        [TestCase(888, "DCCCLXXXVIII")]
+        [TestCase(900, "CM")]
+        [TestCase(1000, "M")]
+        [TestCase(1666, "MDCLXVI")]
+        [TestCase(3999, "MMMCMXCIX")]
+        public void ConvertsNumberToNumeralString(int value, string expected)
+        {
+            var actual = value.ToRomanNumerals();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("I", 1)]
+        [TestCase("II", 2)]
+        [TestCase("III", 3)]
+        [TestCase("IV", 4)]
+        [TestCase("V", 5)]
+        [TestCase("VI", 6)]
+        [TestCase("VII", 7)]
+        [TestCase("VIII", 8)]
+        [TestCase("IX", 9)]
+        [TestCase("X", 10)]
+        [TestCase("XI", 11)]
+        [TestCase("XIV", 14)]
+        [TestCase("XV", 15)]
+        [TestCase("XVI", 16)]
+        [TestCase("XIX", 19)]
+        [TestCase("XX", 20)]
+        [TestCase("XXI", 21)]
+        [TestCase("XXX", 30)]
+        [TestCase("XL", 40)]
+        [TestCase("XLIX", 49)]
+        [TestCase("L", 50)]
+        [TestCase("LI", 51)]
+        [TestCase("LVII", 57)]
+        [TestCase("LXXIV", 74)]
+        [TestCase("XC", 90)]
+        [TestCase("C", 100)]
+        [TestCase("CXXXIX", 139)]
+        [TestCase("CD", 400)]
+        [TestCase("D", 500)]
+        [TestCase("DCLXVI", 666)]
+        [TestCase("DCCCLXXXVIII", 888)]
+        [TestCase("CM", 900)]
+        [TestCase("M", 1000)]
+        [TestCase("MDCLXVI", 1666)]
+        [TestCase("MMMCMXCIX", 3999)]
+
+        public void ConvertsNumeralStringToNumber(string value, int expected)
+        {
+            var actual = value.FromRomanNumerals();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("i", 1)]
+        [TestCase("ii", 2)]
+        [TestCase("iii", 3)]
+        [TestCase("mmmcmxcix", 3999)]
+        public void ConvertsLowerCaseNumeralStringToNumber(string value, int expected)
+        {
+            var actual = value.FromRomanNumerals();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ RomanNumerals/RomanNumeralsConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace RomanNumerals
+{
+    public static class RomanNumeralsConverter
+    {
+        public static string ToRomanNumerals(this int value) =>
+            new string('I', value)
+                .SubstituteGroup('I', 5, 'V', null)
+                .SubstituteGroup('V', 2, 'X', 'I')
+                .SubstituteGroup('X', 5, 'L', 'V')
+                .SubstituteGroup('L', 2, 'C', 'X')
+                .SubstituteGroup('C', 5, 'D', 'L')
+                .SubstituteGroup('D', 2, 'M', 'C');
+
+        private static string SubstituteGroup(this string input, char numeral, int groupSize, char nextNumeral, char? previousNumeral) => groupSize switch
+        {
+            -1 => input.Replace($"{numeral}{previousNumeral}{numeral}", $"{previousNumeral}{nextNumeral}"),
+            2 => input.Replace(new string(numeral, groupSize), nextNumeral.ToString()).SubstituteGroup(numeral, -1, nextNumeral, previousNumeral),
+            4 => input.Replace(new string(numeral, groupSize), $"{numeral}{nextNumeral}"),
+            5 => input.Replace(new string(numeral, groupSize), nextNumeral.ToString()).SubstituteGroup(numeral, 4, nextNumeral, previousNumeral),
+            _ => input
+        };
+
+        public static int FromRomanNumerals(this string value) =>
+            value.Tally('M', 1000, 'C', 100)
+                 .Tally('D', 500, 'C', 100) 
+                 .Tally('C', 100, 'X', 10)
+                 .Tally('L', 50, 'X', 10)
+                 .Tally('X', 10, 'I', 1)
+                 .Tally('V', 5, 'I', 1)
+                 .Length;
+
+        private static string Tally(this string input, char numeral, int score, char deductionNumeral, int deductionScore) =>
+            input.ToUpper()
+                 .Replace($"{deductionNumeral}{numeral}", new string(TALLY_CHARACTER, score - deductionScore))
+                 .Replace(numeral.ToString(), new string(TALLY_CHARACTER, score));
+
+        private const char TALLY_CHARACTER = '|';
+    }
+}
