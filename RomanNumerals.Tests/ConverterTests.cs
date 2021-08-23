7f180b3776298534211c22904785b97d2ce4d448
Dale Palmer <dalepalmer@outlook.com>
2021-08-23 19:32:49 +0100

    Added dictionary implementation.

---
+++ RomanNumerals.Tests/ConverterTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+
+namespace RomanNumerals.Tests
+{
+    public class ConverterTests
+    {
+        [TestCase("XXVI", 26)]
+        [TestCase("MM", 2000)]
+        public void Converter_Convert_WithSimpleNumerals_ReturnsCorrectValue(string input, int expected)
+        {
+            // Assemble
+            var sut = new Converter();
+
+            // Act
+            var actual = sut.Convert(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("IV", 4)]
+        [TestCase("IX", 9)]
+        [TestCase("MCM", 1900)]
+        public void Converter_Convert_WithComplexNumerals_ReturnsCorrectValue(string input, int expected)
+        {
+            // Assemble
+            var sut = new Converter();
+
+            // Act
+            var actual = sut.Convert(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase("xxvi", 26)]
+        [TestCase("mm", 2000)]
+        public void Converter_Convert_WithLowerCaseCaseNumerals_ReturnsCorrectValue(string input, int expected)
+        {
+            // Assemble
+            var sut = new Converter();
+
+            // Act
+            var actual = sut.Convert(input);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ RomanNumerals/Converter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace RomanNumerals
+{
+    public class Converter
+    {
+        private readonly Dictionary<char, int> numerals = new Dictionary<char, int>
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        public int Convert(string input)
+        {
+            var total = 0;
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var current = GetCharValue(i);
+                var next = GetCharValue(i + 1);
+
+                if (current < next)
+                    current = -current;
+
+                total += current;
+            }
+
+            return total;
+
+            int GetCharValue(int position)
+            {
+                if (position >= input.Length)
+                    return 0;
+
+                return numerals[input.ToUpper()[position]];
+            }
+        }
+    }
+}
